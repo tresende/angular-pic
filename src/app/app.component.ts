c79957592a8ac112e08a372e1bda612b669e9856
Thiago Resende <thiago.gcresende@gmail.com>
2018-12-16 00:26:44 -0200

    one way databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pic';
+  title = 'aaa-pic';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg';
 }
