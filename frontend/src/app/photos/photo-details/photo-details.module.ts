edfcd9015d80e46a1448e9e9dcb34d66530b7273
Thiago Resende <thiago.gcresende@gmail.com>
2018-12-30 21:13:05 -0200

    Buscando a foto selecionada da API

--- frontend/src/app/photos/photo-details/photo-details.module.ts
+++ frontend/src/app/photos/photo-details/photo-details.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PhotoDetailsComponent } from './photo-details.component';
 import { CommonModule } from '@angular/common';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
     declarations: [PhotoDetailsComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, PhotoModule],
     exports: [PhotoDetailsComponent]
 })
 export class PhotoDetailsModule {
