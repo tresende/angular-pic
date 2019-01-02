57137c997fe188597aec2601eca677774419ae87
tresende <thiago.gcresende@gmail.com>
2019-01-01 22:37:57 -0200

    Remoção de fotos

--- frontend/src/app/photos/photo-details/photo-details.component.ts
+++ frontend/src/app/photos/photo-details/photo-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
-import { Observable } from 'rxjs';
 
 @Component({
     templateUrl: './photo-details.component.html'
@@ -14,11 +15,18 @@ export class PhotoDetailsComponent implements OnInit {
 
     constructor(
         private route: ActivatedRoute,
-        private photoService: PhotoService
+        private photoService: PhotoService,
+        private router: Router
     ) { }
 
     ngOnInit(): void {
         this.photoId = this.route.snapshot.params.photoId;
         this.photo$ = this.photoService.findById(this.photoId);
+    }
+
+    remove() {
+        this.photoService
+            .removePhoto(this.photoId)
+            .subscribe(() => this.router.navigate(['/']));
     }
 }
