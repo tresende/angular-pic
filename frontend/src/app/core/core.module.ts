4599d65e420803d764314cbee318390a72ae037e
tresende <thiago.gcresende@gmail.com>
2019-01-05 22:09:25 -0200

    Menu da aplicação

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -9,6 +9,7 @@ import { RequestInterceptor } from './auth/request.interceptor';
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
 import { MenuModule } from '../shared/components/menu/menu.module';
+import { ShowIfLoggedModule } from '../shared/components/show-if-logged/show-if-logged.module';
 
 @NgModule({
     declarations: [HeaderComponent, FooterComponent],
@@ -18,7 +19,8 @@ import { MenuModule } from '../shared/components/menu/menu.module';
         RouterModule,
         AlertModule,
         LoadingModule,
-        MenuModule
+        MenuModule,
+        ShowIfLoggedModule
     ],
     providers: [
         {
