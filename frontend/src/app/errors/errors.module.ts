5690284ef14be7d099f53dc74987dcd7341d74d8
tresende <thiago.gcresende@gmail.com>
2019-01-05 23:22:55 -0200

    Stacktrace padronizada entre navegadores

--- frontend/src/app/errors/errors.module.ts
+++ frontend/src/app/errors/errors.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
+import { GlobalErrorComponent } from './global-error/global-error.component';
 
 @NgModule({
-  declarations: [NotFoundComponent],
-  imports: [
-    CommonModule
-  ]
+	declarations: [NotFoundComponent, GlobalErrorComponent],
+	exports: [GlobalErrorComponent],
+	imports: [CommonModule, RouterModule],
+	providers: [
+		{
+			provide: ErrorHandler,
+			useClass: GlobalErrorHandler
+		}
+	]
 })
 export class ErrorsModule { }
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://my-api:3000'
+  apiUrl: 'http://my-api:3000',
+  serverLog: 'http://localhost:7000/'
 };
